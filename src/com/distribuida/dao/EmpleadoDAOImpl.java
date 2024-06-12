eb49655030319313a2cb1f7aa6839cace933811d
MATUTINO <MATUTINO@DESKTOP-SJ72KKT>
2024-06-12 12:32:38 -0500

    nada no vale

--- src/com/distribuida/dao/EmpleadoDAOImpl.java
+++ src/com/distribuida/dao/EmpleadoDAOImpl.java
@@ -1,58 +1,7 @@
-
 package com.distribuida.dao;
 
-import java.util.List;
-
-import javax.transaction.Transactional;
-
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Repository;
-
-import com.distribuida.entities.Empleado;
-
 @Repository
-public class EmpleadoDAOImpl implements EmpleadoDAO {
+public class EmpleadoDAOImpl {
 
-    @Autowired
-    private SessionFactory sessionFactory;
-
-    @Override
-    @Transactional
-    public List<Empleado> findAll() {
-        Session session = sessionFactory.getCurrentSession();
-        return session.createQuery("from Empleado", Empleado.class).getResultList();
-    }
-
-    @Override
-    @Transactional
-    public Empleado findOne(int id) {
-        Session session = sessionFactory.getCurrentSession();
-        return session.get(Empleado.class, id);
-    }
-
-    @Override
-    @Transactional
-    public void add(Empleado empleado) {
-        Session session = sessionFactory.getCurrentSession();
-        session.save(empleado);
-    }
-
-    @Override
-    @Transactional
-    public void update(Empleado empleado) {
-        Session session = sessionFactory.getCurrentSession();
-        session.update(empleado);
-    }
-
-    @Override
-    @Transactional
-    public void delete(int id) {
-        Session session = sessionFactory.getCurrentSession();
-        Empleado empleado = session.byId(Empleado.class).load(id);
-        if (empleado != null) {
-            session.delete(empleado);
-        }
-    }
+	
 }
--- src/com/distribuida/entities/Empleado.java
+++ src/com/distribuida/entities/Empleado.java
@@ -1,22 +1,12 @@
 package com.distribuida.entities;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
-
 import org.springframework.stereotype.Component;
 
 @Component
-@Entity
-@Table(name = "empleado")
+
+
 
 public class Empleado {
-
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
 
 	@Column(name = "id_Empleado")
 	private int idEmpleado;
